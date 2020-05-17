cce7bb90ab6109a9cc3a5404fa577db08d61064b
Yves Navarro <yvesnf@gmail.com>
2020-05-17 13:23:11 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,6 +28,13 @@ app.get('/sxls', function (req, res) {
   res.send('Si Xales!!!!!!!!!!!!!!!!!')
 });
 
+app.post('/demo-angularjs-http-post', function (req, res, body) {
+  
+  res.json(
+      body
+  );
+});
+
 app.get('/spotify/:client_id/:client_secret', (req, resp) => {
 
     let client_id = req.params.client_id;
